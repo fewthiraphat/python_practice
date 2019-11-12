78f97a210cb1384136b9a62a4a92d506aa51326f
Thiraphat Itamonchai <42443713+fewthiraphat@users.noreply.github.com>
2019-11-12 21:46:43 +0700

    Add files via upload

---
+++ solve/mix_number.py
@@ -0,0 +1,164 @@
+q = int(input(""))
+if q > 10:
+    q = int(input(""))
+else:
+    n = int(input(""))
+check = 0
+sum1 = 0
+mix_number = [6,9,12,15,18,20,21,24,26,27,29,30]
+while q<n:
+    q += 1
+    print(q)
+    #normal case
+    if q%mix_number[0]==0:
+        sum1 += 1
+    elif q%mix_number[1]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[2]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[2]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[3]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[6]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[7]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[11]==0:
+        sum1 +=1
+        print("*")
+    #special case
+    elif q%mix_number[3]+mix_number[4]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[3]+mix_number[5]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[3]+mix_number[6]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[3]+mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[3]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[3]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]+mix_number[5]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]+mix_number[6]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]+mix_number[7]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]+mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[4]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]+mix_number[6]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]+mix_number[7]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]+mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[5]+mix_number[11]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[6]+mix_number[7]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[6]+mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[6]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[6]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[6]+mix_number[11]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[7]+mix_number[7]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[7]+mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[7]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[7]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[7]+mix_number[11]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[8]+mix_number[8]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[8]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[8]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[8]+mix_number[11]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[9]+mix_number[9]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[9]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[9]+mix_number[11]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[10]+mix_number[10]==0:
+        sum1 +=1
+        print("*")
+    elif q%mix_number[10]+mix_number[11]==0:
+        sum1 +=1
+        print("*")
+print(sum1)
+
